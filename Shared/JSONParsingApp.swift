f08b1c5eb39c600a1d1847de44d36a7d91ece656
Johnny <romanomess@gmail.com>
2020-08-25 08:53:20 +0300

    Initial Commit

---
+++ Shared/JSONParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParsingApp.swift
+//  Shared
+//
+//  Created by Johnny on 25.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct JSONParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
